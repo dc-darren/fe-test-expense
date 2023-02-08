b08327d5027c7e38c007a234af51c803043717b8
dc-darren <darren.denisson@gmail.com>
2023-02-08 15:12:30 +0700

    Fetch json data and number format fix

--- src/App.js
+++ src/App.js
@@ -1,72 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+
 import Card from "./component/Card";
 
 function App() {
-  const detail1 = [
-    {
-      "jam": "03:15",
-      "tanggal":"18 Februari 2023",
-      "nama":"Ayam Tangkap",
-      "pengeluaran":77249
-    },
-    {
-      "jam": "03:15",
-      "tanggal":"18 Februari 2023",
-      "nama":"Mie Rebus",
-      "pengeluaran":32154
-    },
-  ];
+  const month = "Februari";
+  const year = 2023;
+  const url = "http://localhost:8000/detail";
+  const [detail, setDetail] = useState(null);
+  let total = useRef(0);
 
-  const detail2 = [
-    {
-        "jam": "03:15",
-        "tanggal":"18 Februari 2023",
-        "nama":"Ayam Tangkap",
-        "pengeluaran":77249
-    },
-    {
-        "jam": "03:15",
-        "tanggal":"18 Februari 2023",
-        "nama":"Mie Rebus",
-        "pengeluaran":32154
-    },
-    {
-        "jam": "00:20",
-        "tanggal":"17 Februari 2023",
-        "nama":"Soto Kuning",
-        "pengeluaran":34992
-    },
-    {
-        "jam": "05:59",
-        "tanggal":"17 Februari 2023",
-        "nama":"Ayam Geprek",
-        "pengeluaran":37674
-    },
-    {
-        "jam": "05:45",
-        "tanggal":"16 Februari 2023",
-        "nama":"Otak-otak",
-        "pengeluaran":11971
-    }
-  ];
+  useEffect(() => {
+    total.current = 0;
+
+    fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      // Group object by key of "tanggal"
+      const result = {};
+      for (const {jam, tanggal, nama, pengeluaran} of data) {
+        if (!result[tanggal]) result[tanggal] = [];
+        result[tanggal].push({jam, nama, pengeluaran});
+        total.current += pengeluaran;
+      }
+      setDetail(result);
+    })
+  }, []);
 
   return (
     <>
       <div className="p-4">
         {/* Header */}
         <header className="text-center">
-          <h1 className="text-3xl">Diari Jajan Februari 2023</h1>
-          <p className="text-xl">Pengeluaran Bulan Ini Rp 5.605.475</p>
+          <h1 className="text-3xl">Diari Jajan {month} {year}</h1>
+          <p className="text-xl">Pengeluaran Bulan Ini Rp {(total.current).toLocaleString().replaceAll(',', '.')}</p>
           <button className="bg-violet-800 uppercase text-sm text-white px-3.5 py-1.5 mt-3 smooth-trans hover:bg-violet-900">Tambah Item</button>
         </header>
 
         {/* Cards */}
         <main className="grid grid-cols-12 gap-3 mt-5">
-          <Card detail={detail1} />
-          <Card detail={detail2} />
-          <Card detail={detail1} />
-          <Card detail={detail1} />
-          <Card detail={detail2} />
-          <Card detail={detail1} />
+          {detail && Object.keys(detail).map(key => {
+            return (
+              <Card key={key} year={year} date={key} detail={detail[key]} />
+            );
+          })}
         </main>
       </div>
     </>
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,20 +1,20 @@
-const Card = ({ detail }) => {
+const Card = ({ year, date, detail }) => {
   let total = 0;
 
   return (
     <>
       <div className="col-span-3 text-sm border border-gray-300/50 rounded-sm shadow-[0_1px_4px_0_rgba(0,0,0,0.25)] px-2.5 py-3.5">
-        <h2 className="font-bold text-lg pb-1.5">18 Februari</h2>
+        <h2 className="font-bold text-lg pb-1.5">{date.split(year)}</h2>
         <table className="table-auto w-full">
           <tbody>
-            {detail.map((item) => {
+            {detail.map((item, i) => {
               total += item.pengeluaran;
 
               return (
-                <tr className="border-t border-gray-300/75">
+                <tr key={i} className="border-t border-gray-300/75">
                   <td className="pl-1.5 py-1.5">{item.jam}</td>
                   <td className="pl-1.5 py-1.5">{item.nama}</td>
-                  <td className="text-right whitespace-nowrap pl-1.5 py-1.5">Rp {(item.pengeluaran).toLocaleString().replace(',', '.')}</td>
+                  <td className="text-right whitespace-nowrap pl-1.5 py-1.5">Rp {(item.pengeluaran).toLocaleString().replaceAll(',', '.')}</td>
                 </tr>
               )
             })}
@@ -22,7 +22,7 @@ const Card = ({ detail }) => {
         </table>
         <div className="flex justify-end gap-x-8 font-bold border-t-2 border-gray-300/75 pt-1.5">
           <p>Total</p>
-          <p>Rp {total.toLocaleString().replace(',', '.')}</p>
+          <p>Rp {total.toLocaleString().replaceAll(',', '.')}</p>
         </div>
       </div>
     </>
